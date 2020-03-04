b4c8a975298026b87163b076e3422b10b80fed31
rajha <rajhafajardo@hotmail.com>
2020-03-04 15:57:34 -0300

    first commit

---
+++ BullsEye/AuthorViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AuthorViewController.swift
+//  BullsEye
+//
+//  Created by Rajha Fajardo on 27/02/20.
+//  Copyright © 2020 Rajha Fajardo. All rights reserved.
+//
+
+import UIKit
+
+class AuthorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func close() {
+           dismiss(animated: true, completion: nil)
+       }
+}
